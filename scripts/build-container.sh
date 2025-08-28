dd45b0373ca2323ee7474d135f335ee4e0e6e477
hayao <shun819.mail@gmail.com>
2025-08-29 03:07:43 +0900

    fix: build-container.shでnpmコマンドをbashで実行するように修正

--- scripts/build-container.sh
+++ scripts/build-container.sh
@@ -2,4 +2,4 @@
 
 cd /app || exit 1
 ./scripts/setup-litestream.sh
-exec litestream replicate -config /etc/litestream.yml -exec "npm run migrate && npm run build"
+exec litestream replicate -config /etc/litestream.yml -exec "bash -c 'npm run migrate && npm run build'"
